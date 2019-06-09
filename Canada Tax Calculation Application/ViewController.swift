1744f0fb22210d090a8aa8ec42ff77d680b6abc5
Sara Mazloumi <mz.sara29@gmail.com>
2019-06-09 10:46:15 -0400

    Initial Commit

---
+++ Canada Tax Calculation Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Canada Tax Calculation Application
+//
+//  Created by Sara  on 2019-06-09.
+//  Copyright © 2019 Sara Mazloumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Canada Tax Calculation ApplicationTests/Canada_Tax_Calculation_ApplicationTests.swift
@@ -0,0 +1,34 @@
+//
+//  Canada_Tax_Calculation_ApplicationTests.swift
+//  Canada Tax Calculation ApplicationTests
+//
+//  Created by Sara  on 2019-06-09.
+//  Copyright © 2019 Sara Mazloumi. All rights reserved.
+//
+
+import XCTest
+@testable import Canada_Tax_Calculation_Application
+
+class Canada_Tax_Calculation_ApplicationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
