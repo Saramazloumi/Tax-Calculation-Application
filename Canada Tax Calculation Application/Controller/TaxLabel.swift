88b0dd737b17572dd3fff07650585ca9f8bb6ef7
Sara Mazloumi <mz.sara29@gmail.com>
2019-06-09 12:29:47 -0400

    initial commit

---
+++ Canada Tax Calculation Application/Controller/TaxLabel.swift
@@ -0,0 +1,21 @@
+//
+//  TaxLabel.swift
+//  Canada Tax Calculation Application
+//
+//  Created by Sara  on 2019-06-09.
+//  Copyright © 2019 Sara Mazloumi. All rights reserved.
+//
+
+import UIKit
+
+class TaxLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Canada Tax Calculation Application/Model/Province.swift
@@ -0,0 +1,9 @@
+//
+//  Province.swift
+//  Canada Tax Calculation Application
+//
+//  Created by Sara  on 2019-06-09.
+//  Copyright © 2019 Sara Mazloumi. All rights reserved.
+//
+
+import Foundation
